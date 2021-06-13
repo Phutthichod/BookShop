16062c20b2215e675045c2f2ceb0fe812e2e1dfe
Phutthichod <pin2017create@outlook.com>
2021-06-13 19:20:55 +0700

    Revert "Revert "#1""
    This reverts commit 3dc74c020373e792c6136fde48187f6370b66ea6.

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,7 +1,9 @@
 public class Customer {
+    private int id;
     private String name;
 
-    public Customer(String name) {
+    public Customer(int id,String name) {
+        this.id = id;
         this.name = name;
     }
 
